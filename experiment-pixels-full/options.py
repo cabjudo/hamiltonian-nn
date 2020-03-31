52b08972469a927febf0658ff49171a43eeeea58
Christine <allec@seas.upenn.edu>
2020-03-31 15:40:04 -0400

    Adds full window pixel experiment

---
+++ experiment-pixels-full/options.py
@@ -0,0 +1,28 @@
+import argparse
+
+import os, sys
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PARENT_DIR)
+
+def get_args():
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('--input_dim', default=2*28**2, type=int, help='dimensionality of input tensor')
+    parser.add_argument('--hidden_dim', default=200, type=int, help='hidden dimension of mlp')
+    parser.add_argument('--latent_dim', default=2, type=int, help='latent dimension of autoencoder')
+    parser.add_argument('--learn_rate', default=1e-3, type=float, help='learning rate')
+    parser.add_argument('--input_noise', default=0.0, type=float, help='std of noise added to HNN inputs')
+    parser.add_argument('--batch_size', default=200, type=int, help='batch size')
+    parser.add_argument('--nonlinearity', default='tanh', type=str, help='neural net nonlinearity')
+    parser.add_argument('--total_steps', default=10000, type=int, help='number of gradient steps')
+    parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
+    parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose?')
+    parser.add_argument('--name', default='pixels', type=str, help='either "real" or "sim" data')
+    parser.add_argument('--baseline', dest='baseline', action='store_true', help='run baseline or experiment?')
+    parser.add_argument('--conv', dest='conv', action='store_true', help='use CNN for autoencoder?')
+    parser.add_argument('--seed', default=0, type=int, help='random seed')
+    parser.add_argument('--save_dir', default=THIS_DIR, type=str, help='where to save the trained model')
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.set_defaults(feature=True)
+    return parser.parse_args()
+
